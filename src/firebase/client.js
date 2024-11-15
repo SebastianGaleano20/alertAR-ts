fe5718a3e1e05b04d9ed8dd600d9cd2b96111262
Sebastian Galeano <sebascarp9@gmail.com>
2024-11-15 11:12:49 -0300

    firebase/server typed file

--- src/firebase/client.js
+++ src/firebase/client.js
@@ -1,20 +1,21 @@
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
-// import { getDatabase } from "firebase/database";
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCdgsutSpoFtdtrel-DAoQaBhIrCkIWWOE",
-//   authDomain: "alertar-app.firebaseapp.com",
-//   projectId: "alertar-app",
-//   storageBucket: "alertar-app.firebasestorage.app",
-//   messagingSenderId: "24138897821",
-//   appId: "1:24138897821:web:af8c3777b8e02dd15c3774",
-//   measurementId: "G-BTB0X883BM"
-// };
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const auth = getAuth(app);
-// const db = getDatabase(app);
+const firebaseConfig = {
+    apiKey: "AIzaSyCdgsutSpoFtdtrel-DAoQaBhIrCkIWWOE",
+    authDomain: "alertar-app.firebaseapp.com",
+    projectId: "alertar-app",
+    storageBucket: "alertar-app.firebasestorage.app",
+    messagingSenderId: "24138897821",
+    appId: "1:24138897821:web:af8c3777b8e02dd15c3774",
+    measurementId: "G-BTB0X883BM"
+};
 
-// export { db, auth, app };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getDatabase(app);
+
+export { db, auth, app };
 
