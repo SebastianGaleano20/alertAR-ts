c0f742775aeac650cf38bc9f3f6d4d44bf72885d
SebastianGaleano20 <sebascarp9@gmail.com>
2024-11-28 20:36:34 -0300

    test2

--- e2e/index.spec.ts
+++ e2e/index.spec.ts
@@ -1,7 +1,13 @@
 import { test, expect } from "@playwright/test";
 
-test("has title", async ({ page }) => {
+test("has title AlertAR", async ({ page }) => {
   await page.goto("http://localhost:4321/");
-
   await expect(page).toHaveTitle(/AlertAR/);
+});
+
+test("has title Iniciar Sesión", async ({ page }) => {
+  await page.goto("http://localhost:4321/");
+  await page.getByRole("button", { name: "Iniciar Sesión" }).click();
+  // await page.getByRole("img", { name: "Icono de inicio de sesión" }).click();
+  // await page.getByRole("heading", { name: "Iniciar Sesión" }).click();
 });
